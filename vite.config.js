df8bd436bf04ebe12a4c882cbce507d854a59274
mauroSoliz98 <maurososoliz@gmail.com>
2025-09-05 17:51:29 -0400

    render permission added in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: [
+      'talent-x6sp.onrender.com'
+    ]
+  }
 })
